a5c2b873e617a3871a616cd6b8140c6068c3794a
mizuki-n-2 <mizukizhangze@gmail.com>
2021-09-07 01:37:56 +0900

    🔥未使用関数の削除

--- server/api/tasks/controller.ts
+++ server/api/tasks/controller.ts
@@ -1,8 +1,6 @@
 import { defineController } from './$relay'
 import { getTasks, createTask } from '$/service/tasks'
 
-// const print = (text: string) => console.log(text)
-
 export default defineController({ getTasks }, ({ getTasks }) => ({
   get: async ({ query }) => {
     return { status: 200, body: await getTasks(query.user_id) }
