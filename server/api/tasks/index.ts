a1c6cb051b92a5c168a4c003f9c3a17ceb2e9d28
mizuki-n-2 <mizukizhangze@gmail.com>
2021-09-07 00:30:23 +0900

    💚CI通過のためにコメントアウト, lint

--- server/api/tasks/index.ts
+++ server/api/tasks/index.ts
@@ -1,16 +1,16 @@
 import type { Task } from '$prisma/client'
 
 export type Methods = {
-  // get: {
-  //   query?: {
-  //     limit?: number
-  //     message?: string
-  //   }
+  get: {
+    query?: {
+      limit?: number
+      message?: string
+    }
 
-  //   resBody: Task[]
-  // }
-  // post: {
-  //   reqBody: Pick<Task, 'label'>
-  //   resBody: Task
-  // }
+    resBody: Task[]
+  }
+  post: {
+    // reqBody: Pick<Task, 'label'>
+    resBody: Task
+  }
 }
