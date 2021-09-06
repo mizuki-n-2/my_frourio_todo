2206618031214d39ca0742f5d2a9d9fc314bad24
mizuki-n-2 <mizukizhangze@gmail.com>
2021-09-07 01:18:41 +0900

    ✨タスク取得APIの作成

--- server/api/tasks/controller.ts
+++ server/api/tasks/controller.ts
@@ -1,16 +1,14 @@
-// import { defineController } from './$relay'
-// import { getTasks, createTask } from '$/service/tasks'
+import { defineController } from './$relay'
+import { getTasks, createTask } from '$/service/tasks'
 
 // const print = (text: string) => console.log(text)
 
-// export default defineController({ getTasks, print }, ({ getTasks, print }) => ({
-//   get: async ({ query }) => {
-//     if (query?.message) print(query.message)
-
-//     return { status: 200, body: await getTasks(query?.limit) }
-//   },
-//   post: async ({ body }) => ({
-//     status: 201,
-//     body: await createTask(body.label)
-//   })
-// }))
+export default defineController({ getTasks }, ({ getTasks }) => ({
+  get: async ({ query }) => {
+    return { status: 200, body: await getTasks(query.user_id) }
+  },
+  post: async ({ body }) => ({
+    status: 201,
+    body: await createTask(body.label)
+  })
+}))
--- server/api/tasks/index.ts
+++ server/api/tasks/index.ts
@@ -2,9 +2,8 @@ import type { Task } from '$prisma/client'
 
 export type Methods = {
   get: {
-    query?: {
-      limit?: number
-      message?: string
+    query: {
+      user_id: number
     }
 
     resBody: Task[]
