3d17f4fa0dd51a62a27a8a3e6f283e0a58ab4b03
mizuki-n-2 <mizukizhangze@gmail.com>
2021-09-11 17:41:33 +0900

    Headerコンポーネントの作成

---
+++ components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app-bar
+    app
+    color="blue"
+  >
+
+    <v-icon 
+      large
+      color="white"
+    >
+      mdi-home-analytics
+    </v-icon>
+
+    <v-tabs
+      centered
+      color="white"
+    >
+      <v-tab
+        v-for="link in links"
+        :key="link"
+      >
+        {{ link }}
+      </v-tab>
+    </v-tabs>
+
+    <v-icon
+      color="white"
+    >
+      mdi-logout
+    </v-icon>
+
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator'
+export default class Header extends Vue {
+  private links: Array<string> = [
+    'カンバンボード'
+  ]
+}
+</script>
