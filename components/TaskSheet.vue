a410bc0f0042efccc981297c3967649a6a06c2e6
mizuki-n-2 <mizukizhangze@gmail.com>
2021-09-11 18:39:30 +0900

    タスクシートコンポーネントの作成

---
+++ components/TaskSheet.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-col
+    cols="12"
+    sm="4"
+  >
+    <h3>{{ status }}</h3>
+    <v-sheet
+      min-height="80vh"
+      rounded="lg"
+    >
+      <!--  -->
+    </v-sheet>
+  </v-col>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'TaskSheet',
+  props: {
+    status: { type: String }
+  }
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,38 @@
 <template>
-  
+  <v-app id="inspire">
+    <Header></Header>
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row v-if="taskStatusList">
+          <TaskSheet
+            v-for="(status, index) in taskStatusList"
+            :key="index"
+            :status="status"
+          ></TaskSheet>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
+import Header from '../components/Header.vue'
+import TaskSheet from '../components/TaskSheet.vue'
+
+export default Vue.extend({
+  components: {
+    Header,
+    TaskSheet
+  },
+  data() {
+    return {
+      taskStatusList: [
+        'TODO',
+        'DOING',
+        'DONE'
+      ] 
+    };
+  }
+})
 </script>
