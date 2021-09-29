60d5de94224154123335121b48867ef4f26e487d
mizuki-n-2 <mizukizhangze@gmail.com>
2021-09-30 02:11:14 +0900

    タイムゾーンを日本に変更, but DBには反映されない

--- server/service/tasks.ts
+++ server/service/tasks.ts
@@ -1,4 +1,8 @@
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+dayjs.extend(utc)
+dayjs.extend(timezone)
 import { PrismaClient } from '@prisma/client'
 import { TaskStatus } from '$prisma/client'
 import { CreateTaskRequest } from '$/types'
@@ -8,14 +12,14 @@ const prisma = new PrismaClient()
 export const getTasks = async (userId: number, status?: TaskStatus) =>
   await prisma.task.findMany({ where: { userId, status } })
 
-export const createTask = (userId: number, body: CreateTaskRequest) =>
+export const createTask = (userId: number, body: CreateTaskRequest) => 
   body.status === TaskStatus.DONE
     ? prisma.task.create({
         data: {
           userId,
           title: body.title,
           status: body.status,
-          finishedAt: dayjs().toDate()
+          finishedAt: dayjs().tz('Asia/Tokyo').format()
         }
       })
     : prisma.task.create({
@@ -32,7 +36,7 @@ export const updateTask = (id: number, status: TaskStatus) =>
         where: { id },
         data: {
           status,
-          finishedAt: dayjs().toDate()
+          finishedAt: dayjs().tz('Asia/Tokyo').format()
         }
       })
     : prisma.task.update({
